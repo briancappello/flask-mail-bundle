8b0c3ce97fc1d99106d273a4ce068c0b8a2c76bb
Brian Cappello <briancappello@gmail.com>
2017-12-13 13:22:34 -0500

    rename bundle to mail

--- flask_mail_bundle/__init__.py
+++ flask_mail_bundle/__init__.py
@@ -6,5 +6,5 @@ from .send_mail import send_mail, send_mail_async
 
 class FlaskMailBundle(Bundle):
     module_name = __name__
-    command_group_names = ['mail']
+    name = 'mail'
     extensions_module_name = 'extension'
