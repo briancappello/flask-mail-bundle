40ce62ea28e9c526f3fbf3563cd215c212170235
Brian Cappello <briancappello@gmail.com>
2017-12-16 01:07:02 -0500

    update to flask-unchained

--- flask_mail_bundle/__init__.py
+++ flask_mail_bundle/__init__.py
@@ -1,10 +1,9 @@
-from flask_application_factory import Bundle
+from flask_unchained import Bundle
 
 from .extension import mail
 from .send_mail import send_mail, send_mail_async
 
 
 class FlaskMailBundle(Bundle):
-    module_name = __name__
     name = 'mail'
     extensions_module_name = 'extension'
