1cd385f9d35ef6b71a529df20f0e3d806b228d77
Brian Cappello <briancappello@gmail.com>
2018-03-28 15:01:06 -0400

    fix Mail.__getattr__ so it works outside of the application context

--- flask_mail_bundle/extensions/mail.py
+++ flask_mail_bundle/extensions/mail.py
@@ -1,4 +1,4 @@
-from flask import current_app
+from flask import current_app as app
 from flask_mail import Mail as BaseMail
 
 
@@ -10,4 +10,7 @@ class Mail(BaseMail):
     def __getattr__(self, name):
         # overridden to allow for runtime-modified config values
         state_value = getattr(self.state, name, None)
-        return current_app.config.get(('mail_' + name).upper(), state_value)
+        try:
+            return app.config.get(('mail_' + name).upper(), state_value)
+        except RuntimeError:
+            raise AttributeError(name)
