2dd74ceda1af52ae40a90ec90d9001dba317b970
Brian Cappello <briancappello@gmail.com>
2018-04-07 22:21:31 -0400

    release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='Flask Mail Bundle',
-    version='0.1.0',
+    version='0.2.0',
     description='Adds email sending support to Flask Unchained',
     long_description=long_description,
     url='https://github.com/briancappello/flask-mail-bundle',
